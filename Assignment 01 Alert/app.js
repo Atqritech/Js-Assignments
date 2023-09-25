2790cd4cc6f349b4daf19e266ca39e32a6efa001
Raees Falaksher <atqritech@gmail.com>
2023-09-25 19:33:54 +0500

    h

---
+++ Assignment 01 Alert/app.js
@@ -0,0 +1,11 @@
+alert(" Alert 01 \n welcome to AL Qadri Tech");
+
+alert(" Alert 02 \n Error! Please enter a valid password");
+
+alert(" Alert 03 \n welcome to AL Qadri Tech... \n Happy Coding!");
+
+alert(" Alert 04 \n welcome to AL Qadri Tech...");
+
+alert(" Alert 04 \n Happy coding! \n Prevent this page from creating additional dialogs.");
+
+console.log("Hello... I can runs JS throuh my web browser's console")
